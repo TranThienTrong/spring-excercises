cbe78f4e575354f1d677cd0aa349bf3da73fcba5
Trần Thiên Trọng <36349293+TranThienTrong@users.noreply.github.com>
2021-08-12 18:15:18 +0700

    Upload all Files.

---
+++ excercise2/src/test/java/trong/excercise2/ControllerTest.java
@@ -0,0 +1,26 @@
+package trong.excercise2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk());
+    }
+}
---
+++ excercise2/src/test/java/trong/excercise2/Excercise2ApplicationTests.java
@@ -0,0 +1,13 @@
+package trong.excercise2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Excercise2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
