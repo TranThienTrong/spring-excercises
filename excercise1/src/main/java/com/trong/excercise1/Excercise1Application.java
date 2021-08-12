f4e01901057ebd809c789703ec93db90f6bda820
Trần Thiên Trọng <36349293+TranThienTrong@users.noreply.github.com>
2021-08-12 18:14:35 +0700

    Upload all files.

---
+++ excercise1/src/main/java/com/trong/excercise1/Excercise1Application.java
@@ -0,0 +1,44 @@
+package com.trong.excercise1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@ComponentScan(basePackages={"com.trong"})
+@RestController
+public class Excercise1Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Excercise1Application.class, args);
+
+		String[] beanNames = context.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+	}
+}
